989afd9807d75900aa8a46827fe0babbffa2cbd9
SammyAwesome <sammyhart26@gmail.com>
2016-01-14 11:42:30 -0800

    Update game.js

--- game.js
+++ game.js
@@ -195,7 +195,7 @@ function processKEY(key){
 			}
 		}
 		if(key == 68){
-			filltext("newx = " + newx, 100, 100, makeColor(1, 1, 1, 1.0))
+			filltext("newx = " + newx, 100, 100, makeColor(1, 1, 1, 1.0), "300px Arial", "center", "middle")
 			if(!newx >= screenWidth){
 			newx = newx + flyspeed;
 			flystate = 2;
